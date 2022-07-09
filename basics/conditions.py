fafd4364360c3cef00cec26fae258727b370cab5
Diatemba99 <martin.diatta99@gmail.com>
2022-07-09 13:42:29 +0000

    add of conditions and listes

---
+++ basics/conditions.py
@@ -0,0 +1,8 @@
+temperaturae = 52
+
+if temperaturae <= 27:
+    print('HOT')
+elif temperaturae >=50:
+    print('COLD 2')  
+else:
+    print('SUMMER VIBES')      
---
+++ listes.py
@@ -0,0 +1,5 @@
+produit = ['Sneackers', 'Boots', 'Pick-Up', 'Rolex']
+prix= [12854, 55048, 9896, 5500]
+
+print(produit)
+print(prix)
