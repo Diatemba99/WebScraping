9f00aa5076a2bd9d2fac8a62004c7c94051d7140
Diatemba99 <martin.diatta99@gmail.com>
2022-07-09 12:51:39 +0000

    adding all files

---
+++ basics/exo1.py
@@ -0,0 +1,23 @@
+### 1) Debuggez cette erreur et expliquer brievement le problème en commentaire
+"""
+Nom = 'Sebastien'
+print('mon nom est ',Nom)
+
+"""
+
+
+#2) Faites en sorte que la cellule ci-dessous retourne 'J'apprends le Python' à partir des variables à disposition
+
+"""
+debutPhrase = 'J\'apprends '
+finPhrase = 'le Python'
+print(debutPhrase+finPhrase)
+
+"""
+
+#3) Debugguez le code ci-dessous en modifiant uniquement ce qu'il se trouve dans print(...)
+heuresPassees = 2
+debutPhrase = 'j\'apprends Python depuis '
+finPhrase = ' heures.'
+
+print(debutPhrase+str(heuresPassees)+finPhrase)
