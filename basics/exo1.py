1b9331cb1b4801a0fb153bbfe309830308fe26fc
Diatemba99 <martin.diatta99@gmail.com>
2022-07-09 12:58:11 +0000

    adding exo 4,5

--- basics/exo1.py
+++ basics/exo1.py
@@ -16,8 +16,30 @@ print(debutPhrase+finPhrase)
 """
 
 #3) Debugguez le code ci-dessous en modifiant uniquement ce qu'il se trouve dans print(...)
+"""
 heuresPassees = 2
 debutPhrase = 'j\'apprends Python depuis '
 finPhrase = ' heures.'
 
 print(debutPhrase+str(heuresPassees)+finPhrase)
+"""
+
+#4) quelle sera la valeur de varAge dans le code ci-dessous ? Expliquez en commentaire la raison
+"""
+varAge = 28
+varAge = 30
+print(varAge)
+#la valeur de la variable serait égale à 30, la premiere valeur sera écrasée pour
+#stocker la seconde variable
+"""
+
+
+#5) a) A partir des variables mises à disposition, calculez le taux de conversion et stockez le dans la variable "tauxConversion"
+"""
+nombreDeVisites = 26736352
+nombredeConversions = 973520
+tauxConversion = nombreDeVisites / nombredeConversions
+print(tauxConversion)
+"""
+
+
