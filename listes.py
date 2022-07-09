4355df9b40154d7193ac7f4dc6d94501aebd8db2
Diatemba99 <martin.diatta99@gmail.com>
2022-07-09 14:01:47 +0000

    adding list of lists variables

--- listes.py
+++ listes.py
@@ -1,5 +1,15 @@
 produit = ['Sneackers', 'Boots', 'Pick-Up', 'Rolex']
 prix= [12854, 55048, 9896, 5500]
 
-print(produit)
-print(prix)
+"""print(produit[1:3])
+print(prix[0:2])"""
+
+AllProducts = [['Usb',1250], ['Charger',2580], ['Sofware',9875], ['QWANT', 9634]]
+
+print(AllProducts)
+
+#select element on list 
+print(AllProducts[0])
+
+#select element on list specified
+print(AllProducts[2][:])
