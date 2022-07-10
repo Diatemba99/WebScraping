d84677c3b045cc649743c699484dca3ec27abb92
Diatemba99 <martin.diatta99@gmail.com>
2022-07-10 18:18:30 +0000

    ending loops courses

--- basics/loops.py
+++ basics/loops.py
@@ -8,8 +8,32 @@ while i<=10:
 """
 
 #For loop 
+"""
 produit = ['Sneackers', 'Boots', 'Pick-Up', 'Rolex']
 prix= [12854, 55048, 9896, 5500]
 
 for pd in produit:
     print(pd)
+
+
+AllProducts = {'Usb':1250, 'Charger':2580, 'Sofware':9875, 'QWANT': 9634}
+
+for pd,price in AllProducts.items():
+    print(pd,price)
+    print('*******************')
+    print(f'le prix du {pd} est de {price} ')
+
+
+for i in range(1,10,3):
+    print(i)
+
+for j in range(5,-15,-1):
+    print(j)
+"""
+
+produit = ['Sneackers', 'Boots', 'Pick-Up', 'Rolex']
+
+nbrElement = len(produit)
+
+for i in range(nbrElement):
+    print(produit[i])
