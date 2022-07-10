1bd58a37f4d1347a4aa31d7f1f38c30d151d07a7
Diatemba99 <martin.diatta99@gmail.com>
2022-07-10 14:27:47 +0000

    changing image content on ImageToText script

---
+++ basics/loops.py
@@ -0,0 +1,15 @@
+#While loop testing
+"""
+i=1
+
+while i<=10:
+    print(i)
+    i=i+1
+"""
+
+#For loop 
+produit = ['Sneackers', 'Boots', 'Pick-Up', 'Rolex']
+prix= [12854, 55048, 9896, 5500]
+
+for pd in produit:
+    print(pd)
