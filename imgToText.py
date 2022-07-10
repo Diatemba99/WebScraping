038c2cb1d21fbae1bd4e4040864d5383e4b8272e
Diatemba99 <martin.diatta99@gmail.com>
2022-07-10 01:31:05 +0000

    learning python ascii_art using pywhatkit library

---
+++ imgToText.py
@@ -0,0 +1,25 @@
+import pywhatkit
+
+pywhatkit.image_to_ascii_art('c:\\Users\\Account\\Folder\\imageName.png',
+                            'c:\\Users\\Diatemba\\Pictures\\TxtFileName')
+
+
+"""pywhatkit.image_to_ascii_art('c:\\Users\\Diatemba\\Pictures\\avataars\\avataaars.png',
+                            'c:\\Users\\Diatemba\\Pictures\\avataars\\avataaars')"""
+
+
+print("""\
+
+                                       ._ o o
+                                       \_`-)|_
+                                    ,""       \ 
+                                  ,"  ## |   ಠ ಠ. 
+                                ," ##   ,-\__    `.
+                              ,"       /     `--._;)
+                            ,"     ## /
+                          ,"   ##    /
+
+
+                    """)      
+
+
