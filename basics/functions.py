a4d204485b66a64604d4415e913c4f902af9a5f5
Diatemba99 <martin.diatta99@gmail.com>
2022-07-11 00:46:25 +0000

    adding functions file

---
+++ basics/functions.py
@@ -0,0 +1,9 @@
+def welcomeB(NomUser):
+    print(f'Welcome {NomUser}')
+
+#welcomeB('Cana_Mind')
+def TestingReturn(Prix,TVA):
+    Total = Prix * TVA
+    return Total
+    
+TestingReturn(1523, 50)
