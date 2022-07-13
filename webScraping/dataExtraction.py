0cfc14c34aedefcfd6ed12d62401de6d67efae3e
Diatemba99 <martin.diatta99@gmail.com>
2022-07-13 01:11:19 +0000

    end of data extraction and use of list comprehension

--- webScraping/dataExtraction.py
+++ webScraping/dataExtraction.py
@@ -5,10 +5,26 @@ page =  requests.get("https://quotes.toscrape.com/")
 
 #Verified before printing content
 if page.status_code == 200:
-
-    #specified interpreter for showing code
     parsedPAge = BeautifulSoup(page.content, 'lxml')
 
     #using only element on specified div and class
-    TopTenTagsBox = parsedPAge.find('div',{'class':'col-md-4 tags-box'})
-    TagsOnTagsBox = TopTenTagsBox.find_all('a', {'class':'tag'})
+    citations = parsedPAge.find_all('span', {'class':'text'})
+    #listeCitations = []
+    
+    #loop for addind quotes on list
+    # for cit in citations:
+    #     listeCitations.append(cit.string)
+    # print(listeCitations)    
+    
+    # print('=============================')
+    # val = len(listeCitations)
+    # for i in range(val):
+    #     print(listeCitations[i])
+    
+    
+    print('===========>     LISTE COMPREHENSION    <============')
+    listeCitations = [citation.string for citation in citations]
+    print(listeCitations)
+
+else:
+    print('Contenu introuvable')     
