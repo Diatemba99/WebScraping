ae7eeb1dd5fcf1154c89ad8f4a68bcda474e0d87
Diatemba99 <martin.diatta99@gmail.com>
2022-07-17 23:57:11 +0000

    parsing with html5

--- webScraping/Scrap_Preparation.py
+++ webScraping/Scrap_Preparation.py
@@ -5,7 +5,7 @@ page =  requests.get("https://quotes.toscrape.com/")
 
 #Verified before printing content
 if page.status_code == 200:
-    parsedPAge = BeautifulSoup(page.content, 'lxml')
+    parsedPAge = BeautifulSoup(page.content, 'html5')
 
     #We're going to print only the content of tags
 print(parsedPAge)    
