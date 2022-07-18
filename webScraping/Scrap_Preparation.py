170777ad45db83d236db4c1ffbbdbb1d7b737ecb
Diatemba99 <martin.diatta99@gmail.com>
2022-07-18 02:54:27 +0000

    end of extraction

--- webScraping/Scrap_Preparation.py
+++ webScraping/Scrap_Preparation.py
@@ -1,21 +1,60 @@
 import requests
+import json
 from bs4 import BeautifulSoup
+
 
 page =  requests.get("https://quotes.toscrape.com/")
 
 #Verified before printing content
 if page.status_code == 200:
     parsedPAge = BeautifulSoup(page.content, 'lxml')
-
-    #save all quotes 
     div_citations = parsedPAge.find_all('div', {'class':'quote'})
+   
+    def extract_data(div_citations):
+        citation = div_citations.find('span', {'class':'text'}).get_text()
+        auteur = div_citations.find('small', {'class':'author'}).get_text()
+        tags =[tag.get_text() for tag in div_citations.find_all('a', {'class':'tag'})]
+        data = {
+            'citation' : citation,
+            'auteur' : auteur,
+            'tags' : tags
+        }
+        return data
 
-    #saving separeted values
-    citation = parsedPAge.find('span', {'class':'text'})
-    auteur = parsedPAge.find('small', {'class':'author'})
-    tags = parsedPAge.find_all('a', {'class':'tag'})
+#save content in list
+list_citations = []
+for citation in div_citations:
+    list_citations.append(extract_data(citation))
+#print(list_citations)
 
-    print(citation,auteur,tags)
+
+#saving data in json file
+with open('e:\COURSES\CODES\WEBSCRAPING\EXO\webScraping\quotes.json', 'w') as f:
+    json.dump(list_citations, f)
+
+
+
+
+"""
+============================================================
+============================================================
+============================================================
+"""
+""" citation = div_citations.find('span', {'class':'text'}).get_text()
+    auteur = div_citations.find('small', {'class':'author'}).get_text()
+    #create list comprehension for saving tags on list
+    tags =[tag.get_text() for tag in div_citations.find_all('a', {'class':'tag'})]
+    
+
+    #tags = div_citations.find_all('a', {'class':'tag'})
+    #print(citation,auteur,tags)
+    #create dictionary
+    data = {
+        'citation' : citation,
+        'auteur' : auteur,
+        'tags' : tags
+    }
+    print(data)
 
 
 
@@ -23,4 +62,4 @@ if page.status_code == 200:
     #div_citations = parsedPAge.find_all('div', {'class':'quote'})[0]
     #print(div_citations)
     #print(div_citations)
-
+"""
