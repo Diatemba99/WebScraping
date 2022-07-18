7e248867c664d65bc4deb06149e8c75f3ee5af41
Diatemba99 <martin.diatta99@gmail.com>
2022-07-18 03:36:30 +0000

    end of extraction in one page

---
+++ webScraping/PandasData.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import pandas as pd
+from bs4 import BeautifulSoup
+
+page =  requests.get("https://quotes.toscrape.com/")
+
+#Verified before printing content
+if page.status_code == 200:
+    parsedPAge = BeautifulSoup(page.content, 'lxml')
+    div_citations = parsedPAge.find_all('div', {'class':'quote'})
+   
+    def extract_data(div_citations):
+        citation = div_citations.find('span', {'class':'text'}).get_text()
+        auteur = div_citations.find('small', {'class':'author'}).get_text()
+        tags =[tag.get_text() for tag in div_citations.find_all('a', {'class':'tag'})]
+        data = {
+            'citation' : citation,
+            'auteur' : auteur,
+            'tags' : tags
+        }
+        return data
+
+#save content in list
+list_citations = []
+for citation in div_citations:
+    list_citations.append(extract_data(citation))
+#print(list_citations)
+data_pd = pd.DataFrame.from_dict(list_citations)
+
+#print(data_pd.head(5))
+
+data_pd.to_json('e:\COURSES\CODES\WEBSCRAPING\EXO\webScraping\citations.json')
+data_pd.to_csv('e:\COURSES\CODES\WEBSCRAPING\EXO\webScraping\citations.csv')
+
