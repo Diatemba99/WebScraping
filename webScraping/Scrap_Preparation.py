d6f49305ac8c78bcaa7f2922b8fd80d31548d7bf
Diatemba99 <martin.diatta99@gmail.com>
2022-07-18 00:00:42 +0000

    using two parsed forms

--- webScraping/Scrap_Preparation.py
+++ webScraping/Scrap_Preparation.py
@@ -5,8 +5,11 @@ page =  requests.get("https://quotes.toscrape.com/")
 
 #Verified before printing content
 if page.status_code == 200:
-    parsedPAge = BeautifulSoup(page.content, 'html5')
+    parsedPAge = BeautifulSoup(page.content, 'lxml')
+    parsedPAge2 = BeautifulSoup(page.content, 'html5')
 
     #We're going to print only the content of tags
-print(parsedPAge)    
-print(parsedPAge)    
+print(parsedPAge) 
+print("=="*15)   
+print(parsedPAge2)    
+  
